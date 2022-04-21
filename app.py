ddebf5d55d276b07b9de13b1c6678f82fc7a8c7f
JoyZadan <joyzadan.learning@gmail.com>
2022-04-21 01:16:35 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+"""
+Imports
+"""
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """
+    docstrings
+    """
+    return "Hello World ... again!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
